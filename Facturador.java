022741e0403531843141b2cde02d6a0bbd8dc5cb
falmeidam01 <falmeidam01@educarex.es>
2026-01-19 21:11:00 +0100

    Subida

--- Facturador.java
+++ Facturador.java
@@ -9,7 +9,7 @@ public class Facturador{
 	};
 
 	//Actuaciones realizadas indicando el concierto ofrecido y audiencias obtenidas.
-	static Integer[][] actuaciones = {{0, 20000}, {2, 1200}, {0, 950}, {3, 1140}};
+	static Integer[][] actuaciones = {{0, 20000}, {2, 12000}, {0, 950}, {3, 1140}};
 
 	static String cliente = "Ayuntamiento de Badajoz";
 
